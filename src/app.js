457da829862c792802115ea7cd22584a9f68f1f9
Rafael468 <abadginerrafa@gmail.com>
2025-01-29 19:29:14 +0000

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,56 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+const subjects = [
+  "A random guy",
+  "My friend",
+  "A little bird",
+  "The milkman",
+  "My mother"
+];
+const actions = ["stole", "ate", "broke", "headbutted", "threw away"];
+const objects = ["my homework", "my food", "my keys", "the TV", "your car"];
+const conditions = [
+  "last night",
+  "yesterday",
+  "2 days ago",
+  "during dinner",
+  "while I wasn't looking"
+];
+
+function randomNumber(maxRandomNumber) {
+  return Math.floor(Math.random() * maxRandomNumber);
+}
+
+function randomExcuse() {
+  const randomSubject = subjects[randomNumber(subjects.length)];
+  const randomAction = actions[randomNumber(actions.length)];
+  const randomObject = objects[randomNumber(objects.length)];
+  const randomCondition = conditions[randomNumber(conditions.length)];
+  //another way to concatenate the strings
+  //const excuse = `${randomSubject} ${randomAction} ${randomObject} ${randomCondition}.`;
+
+  return (
+    randomSubject +
+    " " +
+    randomAction +
+    " " +
+    randomObject +
+    " " +
+    randomCondition
+  );
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuseButtonElement = document.getElementById("excuse-button");
+  if (excuseButtonElement) {
+    excuseButtonElement.addEventListener("click", function() {
+      const excuseMessage = document.getElementById("excuse-message");
+      if (excuseMessage) {
+        excuseMessage.innerHTML = randomExcuse();
+      }
+    });
+  } else {
+    console.log("Element with id 'excuse-message' not found.");
+  }
 };
